7b4b06c3bc4f73e823ea1ec2c96113d67e1692b2
Dmitry Bozhko <meridos.programs@yandex.ru>
2024-08-30 16:56:05 +0400

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dvordrova/testTelebot
+
+go 1.22.0
+
+require gopkg.in/telebot.v3 v3.3.6 // indirect
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	pref := tele.Settings{
+		Token:  os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(c tele.Context) error {
+		return c.Send("Hello!")
+	})
+	b.Handle(tele.OnEdited, func(c tele.Context) error {
+		fmt.Println(c.Message().Text)
+		return nil
+	})
+
+	b.Start()
+}
